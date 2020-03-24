5d6736908e2f4ce80fd04c76009e2df3748f6c42
mshanemc <shane.mclaughlin@salesforce.com>
2020-03-24 09:05:35 -0500

    data uploads ok

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,13 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:package:install --package 04t3k0000027AglAAE -w 20
 sfdx force:source:push
+
+# Special perm/license stuff for SustCloud
+sfdx shane:user:psl -l User -g User -n sustain_app_SustainabilityCloudPsl
+sfdx force:user:permset:assign -n SustainabilityAnalytics
+sfdx force:user:permset:assign -n SustainabilityAppAuditor
+sfdx force:user:permset:assign -n SustainabilityAppManager
+sfdx force:user:permset:assign -n SustainabilityCloud
+
+sfdx automig:load -d data --concise
 sfdx force:org:open
