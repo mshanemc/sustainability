647c64f7a9225f57e3d1d3f8655de4297d24c561
mshanemc <shane.mclaughlin@salesforce.com>
2020-04-06 10:35:45 -0500

    temp init/package

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,13 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+# april version
+# sfdx force:package:install --package 04t3k0000027AysAAE -w 20
+
+# march version
 sfdx force:package:install --package 04t3k0000027AglAAE -w 20
 
 # Special perm/license stuff for SustCloud
 sfdx shane:user:psl -l User -g User -n sustain_app_SustainabilityCloudPsl
+sfdx force:user:permset:assign -n RecordTypes 
 sfdx force:user:permset:assign -n SustainabilityAnalytics
 sfdx force:user:permset:assign -n SustainabilityAppAuditor
 sfdx force:user:permset:assign -n SustainabilityAppManager
@@ -11,11 +16,11 @@ sfdx shane:user:psl -l User -g Integration -n InsightsBuilderPsl
 sfdx shane:user:permset:assign -l User -g Integration -n EinsteinAnalyticsUser
 sfdx shane:user:permset:assign -l User -g Integration -n SustainabilityAnalytics
 
-sfdx automig:load -d data --concise
+# sfdx automig:load -d data --concise -m RecordType:DeveloperName
 
-sfdx analytics:app:create -f assets/analyticsAppCreateDef.json
-sfdx shane:analytics:app:share -n Sustainability --org
-sfdx force:source:push
-sfdx force:user:permset:assign -n SustainabilityCustom
+# sfdx analytics:app:create -f assets/analyticsAppCreateDef.json
+# sfdx shane:analytics:app:share -n Sustainability --org
+# sfdx force:source:push
+# sfdx force:user:permset:assign -n SustainabilityCustom
 
-sfdx force:org:open -p /lightning/app/c__Sustainability_Console
+# sfdx force:org:open -p /lightning/app/c__Sustainability_Console
